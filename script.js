5e40c05f90f112e2b72b7d4af611a8f49ee38eda
João Angrezani <jvpb1234@hotmail.com>
2023-06-22 12:24:55 -0300

    Agora deu certo
    Tive dificuldade na orientação a objeto:
    document.getElementById ('visorresultado').innerHTML = (`O total de ${number1} mais ${number2} é ${total}`)

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+function executar() {
     var numero1 = document.getElementById ('primeironumero')
     var numero2 = document.getElementById ('segundonumero')
-    var number1 = numero1 (Number.length)
-    var number2 = numero2 (Number.length)
-    var resultado = number1 + number2
-    console.log = (`${resultado}`)
+    var number1 = Number(numero1.value)
+    var number2 = Number(numero2.value)
+    var total = number1 + number2
+    document.getElementById ('visorresultado').innerHTML = (`O total de ${number1} mais ${number2} é ${total}`)
+}
