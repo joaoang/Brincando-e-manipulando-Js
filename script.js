db41d730602167d79ec3385056c50418ff49dcdf
João Angrezani <jvpb1234@hotmail.com>
2023-06-22 02:33:42 -0300

    salvando antes de executar2

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-var number1 = 5
-var number2 = 5
-var resultado = number1 * number2
-console.log (`5 vezes 5 é ${resultado}`)
-
+function executar () {
+    var numero1 = document.getElementById ('primeironumero')
+    var numero2 = document.getElementById ('segundonumero')
+    var number1 = numero1 (Number.length)
+    var number2 = numero2 (Number.length)
+    var resultado = numero1 + numero2
+    console.log = (`${resultado}`)
+}
