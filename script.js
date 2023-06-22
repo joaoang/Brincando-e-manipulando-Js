a84cedc8a66c0552e2b139198e04c10c06cd4dad
João Angrezani <jvpb1234@hotmail.com>
2023-06-22 02:37:47 -0300

    comitando

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-function executar () {
     var numero1 = document.getElementById ('primeironumero')
     var numero2 = document.getElementById ('segundonumero')
     var number1 = numero1 (Number.length)
     var number2 = numero2 (Number.length)
-    var resultado = numero1 + numero2
+    var resultado = number1 + number2
     console.log = (`${resultado}`)
-}
